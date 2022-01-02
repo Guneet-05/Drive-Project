8c07c7501ff43d483b233f0f98f6c4a9b2488be4
Guneet Malhotra <guneetmalhotra01@gmail.com>
2022-01-02 22:42:18 +0530

    folders added with flex display

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 (function () {
-   alert("Welcome to my Project");
-})
+   
+   let btn = document.querySelector("#createFolderButton");
+   let divContainer = document.querySelector("#container");
+   let myTemplates = document.querySelector("#myTemplates");
+
+   btn.addEventListener("click",function() {
+       let fName = prompt("Enter the name of a folder")
+       
+       if(fName == null) {
+           return;
+       }
+
+       let divFolderTemplate = myTemplates.content.querySelector(".folder");
+       let divFolder = document.importNode(divFolderTemplate,true);
+       
+       divFolder.innerHTML = fName;
+       divContainer.appendChild(divFolder);
+   });
+
+})()
