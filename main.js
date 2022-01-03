d711ef3f087840f907b363a579174849af589a9b
Guneet Malhotra <guneetmalhotra01@gmail.com>
2022-01-03 12:17:59 +0530

    folders preserved in local storage

--- main.js
+++ main.js
@@ -8,60 +8,97 @@
 
 //    load in the beginning
 
-   btn.addEventListener("click",function() {
-       let fName = prompt("Enter the name of a folder")
-       
-       if(fName == null) {
-           return;
-       }
+   btn.addEventListener("click",addFolder);
 
-       let divFolderTemplate = myTemplates.content.querySelector(".folder");
-       let divFolder = document.importNode(divFolderTemplate,true);
-       
-       let divName = divFolder.querySelector("[name='name']")
-       divName.textContent = fName;
+   function addFolder() {
+    let fName = prompt("Enter the name of a folder")
+    
+    if(fName == null) {
+        return;
+    }
+    
+    fid++;
+    addFolderInPage(fName,fid);
+    
+    folders.push({
+        id: fid,
+        name: fName
+    });
+    persistFolders();
+}
 
-       let spanDelete = divFolder.querySelector("[action='delete']");
-
-       spanDelete.addEventListener("click",function() {
-           let flag = confirm("Are you sure you want to delete this folder?" + "Folder: " + divName.textContent);
-           if(flag == false) {
-               return;
-           }
-           divContainer.removeChild(divFolder);
-           let idx = folders.findIndex(f => f.id == parseInt(divFolder.getAttribute("fid")));
-           folders.splice(idx,1);
-           persistFolders();
-       });
-
-       let spanEdit = divFolder.querySelector("[action='edit']");
-
-       spanEdit.addEventListener("click",function() {
-           let fName = prompt("Enter the name of the folder");
-           if(fName == null) {
-               return;
-           }
-
-           divName.textContent = fName;
-           let folder = folders.find(f=> f.id == parseInt(divFolder.getAttribute("fid")));
-           folder.name = fName;
-           persistFolders();
-       })
+   function addFolderInPage(fName,fid) {
+        let divFolderTemplate = myTemplates.content.querySelector(".folder");
+        let divFolder = document.importNode(divFolderTemplate,true);
         
-       divFolder.setAttribute("fid",++fid);
-       divContainer.appendChild(divFolder);
+        let divName = divFolder.querySelector("[name='name']")
+        divName.textContent = fName;
 
-       folders.push({
-           id: fid,
-           name: fName
-       });
-       persistFolders();
-   });
+        let spanDelete = divFolder.querySelector("[action='delete']");
+
+        spanDelete.addEventListener("click",deleteFolder);
+
+        let spanEdit = divFolder.querySelector("[action='edit']");
+
+        spanEdit.addEventListener("click",editFolder);
+        
+        divFolder.setAttribute("fid",fid);
+        divContainer.appendChild(divFolder);
+
+   }
+
+   function deleteFolder() {
+    let divFolder = this.parentNode;
+    let divName = divFolder.querySelector("[name='name']");
+
+    let flag = confirm("Are you sure you want to delete this folder? " + "Folder: " + divName.textContent);
+    if(flag == false) {
+        return;
+    }
+
+    divContainer.removeChild(divFolder);
+    let idx = folders.findIndex(f => f.id == parseInt(divFolder.getAttribute("fid")));
+    folders.splice(idx,1);
+    persistFolders();
+}
+
+   function editFolder() {
+        let fName = prompt("Enter the name of the folder");
+        if(fName == null) {
+            return;
+        }
+        
+        let divFolder = this.parentNode;
+        let divName = divFolder.querySelector("[name='name']")
+
+        divName.textContent = fName;
+        let folder = folders.find(f=> f.id == parseInt(divFolder.getAttribute("fid")));
+        folder.name = fName;
+        persistFolders();
+   }
 
    function persistFolders() {
        console.log(folders);
        let fjson = JSON.stringify(folders);
        localStorage.setItem("data",fjson);
    }
+
+   function loadFoldersFromStorage() {
+       let fjson = localStorage.getItem("data");
+       if(!!fjson) {
+        folders = JSON.parse(fjson);
+        let maxID = -1;
+        folders.forEach(function(f) {
+            addFolderInPage(f.name,f.id);
+            if(f.id > maxID) {
+                maxID = f.id;
+
+            }
+        });
+        fid = maxID;
+       }
+   }
+
+   loadFoldersFromStorage();
 
 })()
